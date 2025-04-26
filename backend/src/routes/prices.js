29cf3406ba3ae1e50800500995f1f05a9d3ccfcb
NavyaSreePenumuchu08 <pnsree0800@gmail.com>
2025-04-26 14:15:08 +0200

    Minor Bug Fix

--- backend/src/routes/prices.js
+++ backend/src/routes/prices.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getPrices } = require('../controllers/priceController');
-
+const {getMonthlyCalendarData} = require('../controllers/priceController')
 router.get('/', getPrices);
 router.get('/calendar', getMonthlyCalendarData);
 
